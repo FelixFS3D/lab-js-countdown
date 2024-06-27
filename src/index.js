a05b107d2cb9514d25ad7fd4233f37332cebae79
Félix Romero <felix.fs3d@gmail.com>
2024-06-27 15:24:35 +0200

    FT-RMT-2406-FELIX

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
+const botonComenzar = document.querySelector("#start-btn")
+let displayCuentaAtras = document.querySelector("#time")
+let tarjeta = document.querySelector("#toast")
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+
+ 
+
+ botonComenzar.addEventListener("click",()=>{
+  startCountdown()
+ })
 
 
 
@@ -15,6 +22,19 @@ let timer = null; // Variable to store the interval
 function startCountdown() {
   console.log("startCountdown called!");
 
+let intervalId = setInterval(()=>{
+  if(remainingTime === 0){
+ return false
+  }
+  remainingTime -- 
+  displayCuentaAtras.innerText = remainingTime
+  if(remainingTime === 0 ){
+    clearInterval(intervalId)
+    showToast();
+    
+  }
+  botonComenzar.disabled = true
+},1000)
 
   // Your code goes here ...
 }
@@ -27,8 +47,11 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
-
-
+setTimeout(()=>{
+  
+  tarjeta.classList.remove("show")
+},3000)
+tarjeta.classList.add("show")
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
